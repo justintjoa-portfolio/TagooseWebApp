17abe93058bf515c7dff4a6e441c5b4cd836dc11
justintjoa <justintoja@gmail.com>
2020-04-19 20:22:34 -0700

    sdf

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 import LoginForm from "../forms/LoginForm";
+import {login} from "../../actions/auth";
+
 //import { Link } from "react-router-dom"
 class Login extends React.Component {
 
-    submit = data => {
-        console.log(data);
-    };
+    submit = data => this.props.login(data).then(() => this.props.history.push("/"));
 
     render() {
         return (
@@ -22,5 +24,14 @@ class Login extends React.Component {
     }
 }
 
+Login.propTypes = {
+    history: PropTypes.shape(
+        {
+            push: PropTypes.func.isRequired
+        }
+    ).isRequired,
+    login: PropTypes.func.isRequired
+};
 
-export default Login
+
+export default connect(null, {login})(Login);
